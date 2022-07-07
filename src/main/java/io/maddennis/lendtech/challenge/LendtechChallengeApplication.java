47c2ed6d9107787b72a7e7cdc318a2e89d923256
dennismasinde <chengwadennismasinde@gmail.com>
2022-07-07 12:56:31 +0300

    db configured

---
+++ src/main/java/io/maddennis/lendtech/challenge/LendtechChallengeApplication.java
@@ -0,0 +1,14 @@
+package io.maddennis.lendtech.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendtechChallengeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LendtechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/maddennis/lendtech/challenge/LendtechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maddennis.lendtech.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendtechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
